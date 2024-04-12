ff3b3337b459c7397bcc2fd89bdd71939d1fcbf2
Zachary Ross <zacharyross1234@gmail.com>
2024-04-12 14:12:10 -0400

    Footer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
     import Header from './header.svelte';
+    import Footer from './footer.svelte';
     import "../app.css";
     import "$lib/styles/main.css";
 </script>
 
 <Header/>
 <slot></slot>
+<Footer/>
