e851881a2b24de097dde5a7aa7dfbec34ad0b298
Zachary Ross <zacharyross1234@gmail.com>
2024-04-12 18:41:11 -0400

    Zine hero and apply

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -14,6 +14,10 @@
         href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=Archivo+Black&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet"
     />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
+        rel="stylesheet"
+    />
 </svelte:head>
 
 <div class="flex dark align-middle h-auto">
