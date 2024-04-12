27b0560df93db95cebbf92c991d90b7c27e7b78f
Zachary Ross <zacharyross1234@gmail.com>
2024-04-12 14:05:12 -0400

    Nav social links

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -17,11 +17,15 @@
 </svelte:head>
 
 <div class="flex dark align-middle h-auto">
-    <div class="flex w-1/6 h-16 justify-center">
-        <img src="assets/shib.png" alt="" class="my-auto h-3/4"/>
+    <div class="flex lg:w-1/6 w-1/4 h-16 justify-evenly">
+        <div class="h-full px-2 flex">
+            <img src="assets/shib.png" alt="" class="my-auto lg:h-3/5 h-4/5 object-contain"/>
+        </div>
+        <div class="h-full px-2 w-16">
+        </div>
     </div>
     {#key $page.url.pathname} <!-- Force re-render-->
-    <nav class="lg:flex w-4/6 h-full justify-evenly my-auto text-center">
+    <nav class="lg:flex lg:w-4/6 w-2/4 h-full justify-evenly my-auto text-center">
         <a href="/">
             <div class="textNav {navColor('/')}">HOME</div>
         </a>
@@ -39,12 +43,16 @@
         </a>
     </nav>
     {/key}
-    <div class="flex w-1/6 h-16 justify-center">
+    <div class="flex lg:w-1/6 w-1/4 h-16 justify-center">
         <div class="h-full px-2 flex">
-            <img src="assets/discord-mark-blue.svg" alt="Discord" class="my-auto h-3/4"/>
+            <a href="https://discord.gg/bb9DqYbeMb" target="_blank" class="my-auto lg:h-3/5 h-4/5">
+                <img src="assets/discord-mark-blue.svg" alt="Discord" class="h-full"/>
+            </a>
         </div>
         <div class="h-full px-2 flex">
-            <img src="assets/insta.svg" alt="Instagram" class=" my-auto h-3/4"/>
+            <a href="https://www.instagram.com/gdyuclub/" target="_blank" class="my-auto lg:h-3/5 h-4/5">
+                <img src="assets/insta.svg" alt="Instagram" class="h-full"/>
+            </a>
         </div>
     </div>
 </div>
