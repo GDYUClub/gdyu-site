a4521001ec018f907118cbd9a479dec7dc2fadfc
Zachary Ross <zacharyross1234@gmail.com>
2024-04-13 17:36:48 -0400

    Flash about component

---
+++ src/routes/zine/flashAbout.svelte
@@ -0,0 +1,83 @@
+<div class="dark w-full">
+
+    <div class="flex h-20 ml-10">
+        <div class="pinkText blockTopText my-auto">ABOUT</div>
+    </div>
+
+    <div class="flex">
+        <div class="flex flex-col w-1/12">
+            
+        </div>
+
+        <div class="w-10/12">
+            <div class="pinkText blockHeaderText">
+                FLASH!
+            </div>
+
+            <p class="mt-1 blueText">
+                WHAT IS IT?
+            </p>
+
+            <p class="mt-10">
+                FLASH! publishes short stories and articles about games and game development, online at gdyu.club and as a physical copy every 4 months. Stories and articles are divided into 3 groups:
+            </p>
+            
+            <div class="flex gap-4 flex-wrap mt-10 justify-center">
+                <div class="flashCard w-96 flex flex-col">
+                    <p class="darkText cardTitleText text-center pt-4">TABS</p>
+                    <p class="pinkText cardText text-center pt-4">[150-300 Words]</p>
+                    <p class="darkText cardText text-center pt-4 px-4">The shortest articles in FLASH!, Tabs are for little thoughts about the games you've played; moments you loved, characters you connected to, places you remember</p>
+                    <img class="w-1/2 mx-auto py-4" src="assets/tabs.svg" alt="">
+                </div>
+                <div class="flashCard w-96 flex flex-col">
+                    <p class="darkText cardTitleText text-center pt-4">PAGES</p>
+                    <p class="pinkText cardText text-center pt-4">[300-700 Words]</p>
+                    <p class="darkText cardText text-center pt-4 px-4">A good place for longer thoughts, Pages are for short personal essays about your experiences in games</p>
+                    <img class="w-1/2 mx-auto py-4 mt-auto" src="assets/pages.svg" alt="">
+                </div>
+                <div class="flashCard w-96 flex flex-col">
+                    <p class="darkText cardTitleText text-center pt-4">WINDOWS</p>
+                    <p class="pinkText cardText text-center pt-4">[500-700 Words]</p>
+                    <p class="darkText cardText text-center pt-4 px-4">With a different focus than other stories, Windows are for writing about the game industry and game development</p>
+                    <img class="w-1/2 mx-auto py-4 mt-auto" src="assets/windows.svg" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col w-1/12">
+            <div class="yellow dot mx-auto -mb-1"/>
+            <div class="yellow w-0.5 mx-auto grow"/>
+            <div class="yellow diamond mx-auto -mt-1"/>
+        </div>
+    </div>
+
+    <div class="flex w-1/6 h-20">
+        <div class="yellow dot my-auto -mr-1 ml-10"/>
+        <div class="yellow h-0.5 my-auto grow"/>
+        <div class="yellow diamond my-auto -ml-1"/>
+    </div>
+</div>
+
+<style>
+    .test {
+        bottom: 0;
+    }
+    .cardTitleText {
+        font-family: 'Archivo Black';
+        font-weight: 400;
+        font-size: 40px;
+        line-height: 40px;
+    }
+    .cardText {
+        font-family: 'Almarai';
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 24px;
+    }
+
+    .flashCard {
+        background-color: #F5D33D;
+        border-radius: 10px;
+        overflow: hidden;
+    }
+</style>
