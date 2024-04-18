21614ccb04bc0b377c629a1a5bac67beea5e9166
Zachary Ross <zacharyross1234@gmail.com>
2024-04-18 11:25:09 -0400

    Revert css fix attempts

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -23,7 +23,6 @@
 </script>
 
 <svelte:head>
-    <link rel="stylesheet" href="global.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet"
