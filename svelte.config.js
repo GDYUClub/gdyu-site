3e2dd6a27e7d2a9c6643459bbaf31e695f42b53c
JUB_Yoush <yoush1337@gmail.com>
2024-04-22 19:44:21 -0400

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import {mdsvex} from 'mdsvex'
 import sveltePreprocess  from 'svelte-preprocess';
 
@@ -9,16 +9,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages:'build',
-			assets:'build',
-			fallback: 'app.html',
-			precompress: false,
-			strict:true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter(),
 	},
 	extensions:['.svelte','.md'],
 
