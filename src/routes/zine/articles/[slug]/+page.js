28068e5723b4438dc51f0499fafcefe7e19a057f
Zachary Ross <zacharyross1234@gmail.com>
2024-08-16 22:38:26 -0400

    Fix zine lowercase slug

--- src/routes/zine/articles/[slug]/+page.js
+++ src/routes/zine/articles/[slug]/+page.js
@@ -1,6 +1,6 @@
 //pulls all the posts before the svelte page is loaded
 export async function load({ params, fetch }) {
-	const post = await import(`../${params.slug}.md`);
+	const post = await import(`../${params.slug.toUpperCase()}.md`);
 	const { title, author, date, cover } = post.metadata;
 	const content = post.default;
 
