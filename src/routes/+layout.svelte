27dac74e53af72dc533c2509b138d92aae7b8e28
JUB_Yoush <yoush1337@gmail.com>
2025-05-27 15:25:35 -0400

    static export

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+    export const prerender = true;
     import Header from './header.svelte';
     import Footer from './footer.svelte';
     import "../app.css";
--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,27 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
-import {mdsvex} from 'mdsvex'
-import sveltePreprocess  from 'svelte-preprocess';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
+import { mdsvex } from "mdsvex";
+import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-	},
-	extensions:['.svelte','.md'],
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
+  },
+  extensions: [".svelte", ".md"],
 
-	preprocess: [
-		sveltePreprocess({ postcss: true }),
-		mdsvex({
-			extensions:['.md'],
-		})
-	]
+  preprocess: [
+    sveltePreprocess({ postcss: true }),
+    mdsvex({
+      extensions: [".md"],
+    }),
+  ],
 };
 
 export default config;
