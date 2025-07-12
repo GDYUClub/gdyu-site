7098cda41c4398bc82b4f85049b16dd8a9037413
Anheiy <isholatarka@gmail.com>
2025-07-12 18:25:40 -0400

    svelte config change

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.argv.includes("gdyu-site") ? "" : process.env.BASE_PATH,
+      base: process.env.NODE_ENV === 'production' ? '/gdyu-site' : ''
     },
   },
   extensions: [".svelte", ".md"],
